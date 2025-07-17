e06fbdf1c1ff5ffdb0a5b5cfdc3229b57b27f67a
Riddhikapaliwal <riddhikahere2005@gmail.com>
2025-07-17 18:44:52 +0530

    update

--- numberguessing.py
+++ numberguessing.py
@@ -1,5 +1,4 @@
 #use ctrl + shift + M....to find problems in vs code workspace
-
 import random
 
 print('**Here is the NNUMBER GUESSING GAME \n')
@@ -8,28 +7,23 @@ print('Game will continue till you guess the no correctly....\n')
 print('if you want to quit enter "quit" ')
 
 
-orignal=random.randint(1,51)
-
 print('So lets BEGIN')
-print('Enter your Guess')
 
-guess=input()
+while True: 
+    original=random.randint(1,51)
+    guess=input('Enter your guess\n')
+    print(f'\n my guess is {original}')
+    if guess==original:
+        print('you won\n')
 
-if guess==orignal:
-    print('you won\n')
+    elif int(guess)>50:
+        print('enter the number in range\n')
 
-elif guess>50:
-    print('enter the number in range\n')
+    elif guess != original:
+        print('try again \n')
 
-elif guess=='quit':
-    print('thanks....successfully quitted')
-
-elif guess != orignal:
-    print('try again')
-
-else:
-    print('entera valid input')
+    else:
+        print('entera valid input\n')
     
-
 
 
